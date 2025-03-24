9c148748634fe008e6822a26759950c6d702efcf
zohu <38601989@qq.com>
2025-03-24 08:09:04 +0800

    add: zdb支持地理坐标，且支持wgs84/gcj02/bd09

--- zdb/type_point.go
+++ zdb/type_point.go
@@ -3,20 +3,48 @@ package zdb
 import (
 	"context"
 	"database/sql/driver"
+	"encoding/hex"
 	"fmt"
 	"github.com/twpayne/go-geom"
 	"github.com/twpayne/go-geom/encoding/ewkb"
+	"github.com/zohu/zfiber/zutil"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 	"gorm.io/gorm/schema"
 )
 
-// Point
-// @Description: ewkb：专为 PostGIS 设计，简化了 SRID 和扩展类型的处理，是数据库交互的首选。
-type Point ewkb.Point
+type PointType string
 
-func NewPoint(x, y float64) Point {
-	return Point{Point: geom.NewPointFlat(geom.XY, []float64{x, y})}
+const (
+	PointTypeWGS84 PointType = "wgs84"
+	PointTypeGCJ02 PointType = "gcj02"
+	PointTypeBD09  PointType = "bd09"
+)
+
+// Point
+// @Description: 数据库只存储WGS84
+type Point struct {
+	ewkb.Point
+	Longitude float64   `json:"longitude,omitempty"`
+	Latitude  float64   `json:"latitude,omitempty"`
+	PointType PointType `json:"point_type"` // 仅仅用于描述Longitude和Latitude的类型，ewkb.Point一直都是WGS84
+}
+
+func NewPointFromWGS84(x, y float64) *Point {
+	return &Point{
+		Point: ewkb.Point{
+			Point: geom.NewPointFlat(geom.XY, []float64{x, y}),
+		},
+		Longitude: x,
+		Latitude:  y,
+		PointType: PointTypeWGS84,
+	}
+}
+func NewPointFromGCJ02(x, y float64) *Point {
+	return NewPointFromWGS84(zutil.GCJ02toWGS84(x, y))
+}
+func NewPointFromDB09(x, y float64) *Point {
+	return NewPointFromWGS84(zutil.BD09toWGS84(x, y))
 }
 
 func (p *Point) Equal(other Point) bool {
@@ -24,15 +52,7 @@ func (p *Point) Equal(other Point) bool {
 }
 
 func (p *Point) GormDBDataType(db *gorm.DB, field *schema.Field) string {
-	return "GEOMETRY(Point,4326)"
-}
-
-// GormValue INSERT INTO `users` (`name`,`point`) VALUES ("jinzhu",ST_PointFromText("POINT(100 100)"))
-func (p *Point) GormValue(ctx context.Context, db *gorm.DB) clause.Expr {
-	return clause.Expr{
-		SQL:  "ST_PointFromText(?)",
-		Vars: []interface{}{fmt.Sprintf("POINT(%f %f)", p.X(), p.Y())},
-	}
+	return "GEOGRAPHY(Point)"
 }
 
 func (p *Point) Scan(value interface{}) error {
@@ -40,10 +60,72 @@ func (p *Point) Scan(value interface{}) error {
 		*p = Point{}
 		return nil
 	}
-	b := ewkb.Point(*p)
-	return b.Scan(value)
+	t, err := hex.DecodeString(value.(string))
+	if err != nil {
+		return err
+	}
+	err = p.Point.Scan(t)
+	if err != nil {
+		return err
+	}
+	p.Longitude = p.X()
+	p.Latitude = p.Y()
+	p.PointType = PointTypeWGS84
+	return nil
 }
-func (p Point) Value() (driver.Value, error) {
-	b := ewkb.Point(p)
-	return b.Value()
+func (p *Point) Value() (driver.Value, error) {
+	return p.Point.Value()
+}
+func (p *Point) GormValue(ctx context.Context, db *gorm.DB) clause.Expr {
+	return clause.Expr{
+		SQL:  "ST_GeographyFromText(?)",
+		Vars: []interface{}{fmt.Sprintf("POINT(%f %f)", p.X(), p.Y())},
+	}
+}
+
+// GCJ02
+// @Description: 高德、腾讯、阿里、google地图 坐标系
+// @receiver p
+// @return x
+// @return y
+func (p *Point) GCJ02() *Point {
+	switch p.PointType {
+	case PointTypeWGS84:
+		p.Longitude, p.Latitude = zutil.WGS84toGCJ02(p.X(), p.Y())
+	case PointTypeBD09:
+		p.Longitude, p.Latitude = zutil.BD09toGCJ02(p.X(), p.Y())
+	}
+	p.PointType = PointTypeGCJ02
+	return p
+}
+
+// BD09
+// @Description: 百度坐标系
+// @receiver p
+// @return x
+// @return y
+func (p *Point) BD09() *Point {
+	switch p.PointType {
+	case PointTypeWGS84:
+		p.Longitude, p.Latitude = zutil.WGS84toBD09(p.X(), p.Y())
+	case PointTypeGCJ02:
+		p.Longitude, p.Latitude = zutil.GCJ02toBD09(p.X(), p.Y())
+	}
+	p.PointType = PointTypeBD09
+	return p
+}
+
+// WGS84
+// @Description: WGS84坐标系
+// @receiver p
+// @return *Point
+func (p *Point) WGS84() *Point {
+	switch p.PointType {
+	case PointTypeGCJ02:
+		p.Longitude, p.Latitude = zutil.GCJ02toWGS84(p.X(), p.Y())
+	case PointTypeBD09:
+		p.Longitude, p.Latitude = zutil.BD09toWGS84(p.X(), p.Y())
+	}
+	p.PointType = PointTypeWGS84
+	return p
 }
